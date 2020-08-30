176951edc5acd4a5ae103eba4b435aea4136251d
rayanah <66557005+rayanah@users.noreply.github.com>
2020-08-30 22:03:46 +0300

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-k3d cluster create jenkins -v /var/run/docker.sock:/var/run/docker.sock --agents 3 -p 30000-32767:30000-32767@server[0]
-source <(k3d completion bash)
-source <(kubectl completion bash)
 kubectl apply -f jenkins.namespace.yaml
 kubectl apply -f jenkins.helm.yaml
 WAIT=60
